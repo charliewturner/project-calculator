5b8e45f0858b74e22c0368070398374024c1cebf
Charlie <c.w.turner@hotmail.com>
2023-11-04 16:25:57 +0000

    Add button references to JS file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const plusButton = document.getElementById("plusButton");
+const minusButton = document.getElementById("minusButton");
+const divideButton = document.getElementById("divideButton");
+const multiplyButton = document.getElementById("multiplyButton");
+const equalsButton = document.getElementById("equalsButton");
+
 let firstNum = 0;
 let secondNum = 0;
 let operator;
@@ -25,10 +31,12 @@ function multiply (firstNum, secondNum) {
     return result;
 }
 
+function operate () {
+    operator = 
+}
+
+
 function clearAll () {
     
 }
 
-function operate () {
-    
-}
